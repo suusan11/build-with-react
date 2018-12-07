f5a57e66d1cbfc08f340256d140f66227253d479
suusan11 <gawow11@gmail.com>
2018-12-06 21:31:58 -0800

    created Exercise Event folder

---
+++ Exercise-Event/script.js
@@ -0,0 +1,30 @@
+var ChildComponent = React.createClass({
+    render: function() {
+        return(
+            <div>
+                <div className="prompt">Add a click handler to this button so that when clicked, performMagic is called in the parent component.</div>
+                <button onClick={this.props.onMagicClick}>Do Magic</button>
+            </div>
+        )
+    }
+});
+
+
+var ParentComponent = React.createClass({
+    performMagic: function() {
+        alert('TAADAH!');
+    },
+
+    render: function() {
+        return(
+            <div>
+                <ChildComponent onMagicClick={this.performMagic} />
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <ParentComponent />,
+    document.getElementById("container");
+);
